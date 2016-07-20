5e4fcf3497039c0705a818a0f3aa27a500241de0
LiuchangDuan <870121425@qq.com>
2016-07-20 15:21:38 +0800

    自定义相机

---
+++ app/src/main/java/com/example/camerademo/CustomCamera.java
@@ -0,0 +1,153 @@
+package com.example.camerademo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2016/7/19.
+ */
+public class CustomCamera extends Activity implements SurfaceHolder.Callback {
+
+    private Camera mCamera;
+
+    private SurfaceView mPreView;
+
+    private SurfaceHolder mHolder;
+
+    private Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File tempFile = new File("/sdcard/temp.png");
+            try {
+                FileOutputStream fos = new FileOutputStream(tempFile);
+                fos.write(data);
+                fos.close();
+                Intent intent = new Intent(CustomCamera.this, ResultAty.class);
+                intent.putExtra("picPath", tempFile.getAbsolutePath());
+                startActivity(intent);
+                CustomCamera.this.finish();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom);
+//        mCamera = Camera.open();
+        mPreView = (SurfaceView) findViewById(R.id.preview);
+        mHolder = mPreView.getHolder();
+        mHolder.addCallback(this);
+        mPreView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.autoFocus(null);
+            }
+        });
+    }
+
+    public void capture(View view) {
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPictureFormat(ImageFormat.JPEG);
+        parameters.setPreviewSize(4000, 3000);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        mCamera.autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean success, Camera camera) {
+                if (success) {
+                    mCamera.takePicture(null, null, mPictureCallback);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mCamera == null) {
+            mCamera = getCamera();
+            if (mHolder != null) {
+                setStartPreview(mCamera, mHolder);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    /**
+     * 获取Camera对象
+     * @return
+     */
+    private Camera getCamera() {
+        Camera camera;
+        try {
+            camera = Camera.open();
+        } catch (Exception e) {
+            camera = null;
+            e.printStackTrace();
+        }
+        return camera;
+    }
+
+    /**
+     * 开始预览相机内容
+     */
+    private void setStartPreview(Camera camera, SurfaceHolder holder) {
+        try {
+            camera.setPreviewDisplay(holder);
+            // 默认为横屏，将横屏转化为竖屏
+            // 将系统Camera预览角度进行调整
+            camera.setDisplayOrientation(90);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 释放相机资源
+     */
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        setStartPreview(mCamera, holder);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mCamera.stopPreview();
+        setStartPreview(mCamera, mHolder);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        releaseCamera();
+    }
+}
